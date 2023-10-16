31919a864c7d5b4ce34a10eedb05289a9f8d8dfa
Hugo-Hattori <136493140+Hugo-Hattori@users.noreply.github.com>
2023-10-16 15:27:09 -0300

    implementing url_for

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
@@ -14,7 +14,7 @@ def contato():
     return render_template('contato.html')
 
 
-@app.route('/usuarios')
+@app.route('/lista-usuarios')
 def usuarios():
     return render_template('usuarios.html', lista_usuarios=lista_usuarios)
 
