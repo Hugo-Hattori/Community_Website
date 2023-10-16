9e68a322defd5bfae6424350f70fd750099787e5
Hugo-Hattori <136493140+Hugo-Hattori@users.noreply.github.com>
2023-10-16 18:28:41 -0300

    creating forms.py

---
+++ form.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
+
+
+class FormCriarConta(FlaskForm):
+    username = StringField('Nome de Usuário', validators=[DataRequired()])
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    confirmacao_senha = PasswordField('Confirme a Senha', validators=[DataRequired(), EqualTo('senha')])
+    botao_submit_crianconta = SubmitField('Criar Conta')
+
+
+class FormLogin(FlaskForm):
+    email = StringField('E-mail', validators=[DataRequired(), Email()])
+    senha = PasswordField('Senha', validators=[DataRequired(), Length(6, 20)])
+    botao_submit_login = SubmitField('Fazer Login')
