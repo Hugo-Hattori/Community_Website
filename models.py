3c405201bed35d0d37c7b00da6a0da7f83add6c7
Hugo-Hattori <136493140+Hugo-Hattori@users.noreply.github.com>
2023-10-20 18:23:01 -0300

    relationship between classes
    Establishing relationship between Usuario and Post classes

--- models.py
+++ models.py
@@ -8,6 +8,8 @@ class Usuario(database.Model):
     email = database.Column(database.String, nullable=False, unique=True)
     senha = database.Column(database.String, nullable=False)
     foto_perfil = database.Column(database.String, default='default.jpg')
+    posts = database.relationship('Post', backref='autor', lazy=True)
+    cursos = database.Column(database.String, nullable=False, default='Não Informado')
 
 
 class Post(database.Model):
@@ -15,3 +17,4 @@ class Post(database.Model):
     titulo = database.Column(database.String, nullable=False)
     corpo = database.Column(database.Text, nullable=False)
     data_criacao = database.Column(database.DateTime, nullable=False, default=datetime.utcnow)
+    id_usuario = database.Column(database.Integer, database.ForeignKey('usuario.id'), nullable=False)
