44c6f31560be5c0fc6bbd324925f13adea1614ac
Hugo-Hattori <136493140+Hugo-Hattori@users.noreply.github.com>
2023-10-23 16:51:33 -0300

    implementing login_manager

--- comunidade_impressionadora/routes.py
+++ comunidade_impressionadora/routes.py
@@ -3,6 +3,7 @@ from flask import render_template, redirect, url_for, flash, request
 from comunidade_impressionadora.forms import FormLogin, FormCriarConta
 from translate import Translator
 from comunidade_impressionadora.models import Usuario
+from flask_login import login_user
 
 
 lista_usuarios = ['Hugo', 'Alexandre', 'Renan', 'Amanda']
@@ -31,8 +32,13 @@ def login():
     form_criarconta = FormCriarConta()
 
     if form_login.validate_on_submit() and 'botao_submit_login' in request.form: #verifica se o form é válido e qual botão foi clicado
-        flash(f'Login bem sucedido no e-mail: {form_login.email.data}', 'alert-success')
-        return redirect(url_for('home'))
+        usuario = Usuario.query.filter_by(email=form_login.email.data).first()
+        if usuario and bcrypt.check_password_hash(usuario.senha, form_login.senha.data):
+            login_user(usuario, remember=form_login.lembrar_logado.data)
+            flash(f'Login bem sucedido no e-mail: {form_login.email.data}', 'alert-success')
+            return redirect(url_for('home'))
+        else:
+            flash('Falha no login. E-mail ou senha incorretos.', 'alert-danger')
 
     if form_criarconta.validate_on_submit() and 'botao_submit_crianconta' in request.form: #verifica se o form é válido e qual botão foi clicado
         senha_cript = bcrypt.generate_password_hash(form_criarconta.senha.data)
