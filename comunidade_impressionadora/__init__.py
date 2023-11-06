a603f27d2d9eef7b639fdbe85296b43485e1caaa
Hugo-Hattori <136493140+Hugo-Hattori@users.noreply.github.com>
2023-11-06 19:54:44 -0300

    Adjustments for deploy
    Database related adjustments for deploy.

--- comunidade_impressionadora/__init__.py
+++ comunidade_impressionadora/__init__.py
@@ -3,6 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
+import sqlalchemy
 
 
 app = Flask(__name__)
@@ -12,12 +13,25 @@ if os.getenv('DATABASE_URL'):
 else:
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///comunidade.db'
 
+
 database = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view = 'login' #redirecionado páginas que requerem login para a página da função login
 login_manager.login_message = 'Faça login para continuar.'
 login_manager.login_message_category = 'alert-info'
+
+
+from comunidade_impressionadora import models #isto é ncessário para criar as tabelas para o deploy
+engine = sqlalchemy.create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+inspector = sqlalchemy.inspect(engine)
+if not inspector.has_table('usuario'):
+    with app.app_context():
+        database.drop_all()
+        database.create_all()
+        print('Base de Dados criada.')
+else:
+    print('Base de Dados já existente.')
 
 
 from comunidade_impressionadora import routes
